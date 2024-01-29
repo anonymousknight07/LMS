3f373b2a68be677bdeb994d3149235bc79eaffe3
Akshat <akshath.centaurus@gmail.com>
2024-01-29 19:58:16 +0530

    made the login page

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-3xl font-bold underline'>
-      Hello world!
-    </p>
+    <div>
+      <UserButton 
+        afterSignOutUrl="/">
+          
+        </UserButton>
+    </div>
   )
 }
