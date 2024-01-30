e2c4867126be0d4b0c54bc1e58926191616a6368
Akshat <akshath.centaurus@gmail.com>
2024-01-30 20:19:34 +0530

    Create course option for admin added

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,30 +1,30 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-import { db } from "@/lib/db";
+// import { db } from "@/lib/db";
 
-import { DataTable } from "./_components/data-table";
-import { columns } from "./_components/columns";
+// import { DataTable } from "./_components/data-table";
+// import { columns } from "./_components/columns";
 
 const CoursesPage = async () => {
-  const { userId } = auth();
+//   const { userId } = auth();
 
-  if (!userId) {
-    return redirect("/");
-  }
+//   if (!userId) {
+//     return redirect("/");
+//   }
 
-  const courses = await db.course.findMany({
-    where: {
-      userId,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+//   const courses = await db.course.findMany({
+//     where: {
+//       userId,
+//     },
+//     orderBy: {
+//       createdAt: "desc",
+//     },
+//   });
 
   return ( 
     <div className="p-6">
-      <DataTable columns={columns} data={courses} />
+      {/* <DataTable columns={columns} data={courses} /> */}
     </div>
    );
 }
